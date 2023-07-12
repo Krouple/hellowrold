b274e0416f5610c43272631685b989c018ec5371
Krouple <94224042+Krouple@users.noreply.github.com>
2023-07-12 19:44:59 +0800

    test third commit again

--- src/com/mygit/Main.java
+++ src/com/mygit/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Git I am coming! third");
+        System.out.println("Git I am coming! second");
     }
 }
